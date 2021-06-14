1d342b4a294a9d0190139f9c1b581624fdc11071
Lemonwaterpool <frqk3624@gmail.com>
2021-06-14 23:02:59 +0900

    Initial Commit

---
+++ Shared/SwiftUI_PayPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PayPayApp.swift
+//  Shared
+//
+//  Created by H Chan on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PayPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
